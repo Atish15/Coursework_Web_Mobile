970f9b261074f2fdd2ad7b6a555e96ee91fdbadc
aatishgyawali <aatishgyawali10@gmail.com>
2022-11-18 15:26:38 +0000

    Adding a product array js file

---
+++ lesson.js
@@ -0,0 +1,85 @@
+
+    let  lessonproduct= [
+        {
+        id: '1001',
+        subject: 'Maths',
+        location: 'London',
+        price: ' 100',
+        count: 5,
+        imagesrc: 'images/maths.png'
+    },
+        {
+            id: '1002',
+            subject: 'Music',
+            location: 'London',
+            price: ' 150',
+            count: 5,
+            imagesrc: 'images/music.png'
+        },
+        {
+            id: '1003',
+            subject: 'English',
+            location: 'Oxford',
+            price: '90',
+            count: 5,
+            imagesrc: 'images/eng.png'
+        },
+        {
+            id: '1004',
+            subject: 'Maths',
+            location: 'Manchester',
+            price:  '100',
+            count: 5,
+            imagesrc: 'images/maths.png'
+        },
+        {
+            id: '1005',
+            subject: 'Computer',
+            location: 'London',
+            price: ' 170',
+            count: 10,
+            imagesrc: 'images/computer.png'
+        },
+        {
+            id: '1006',
+            subject: 'Sports',
+            location: 'London',
+            price: ' 300',
+            count: 15,
+            imagesrc: 'images/sports.png'
+        },
+        {
+            id: '1007',
+            subject: 'English',
+            location: 'Liverpool',
+            price: ' 60',
+            count: 7,
+            imagesrc: 'images/eng.png'
+        },
+        {
+            id: '1008',
+            subject: 'Physics',
+            location: 'Brighton',
+            price: '110',
+            count: 5,
+            imagesrc: 'images/physics.png'
+        },
+        {
+            id: '1009',
+            subject: 'Sports',
+            location: 'Manchester',
+            price: ' 300',
+            count: 15,
+            imagesrc: 'images/sports.png'
+        },
+        {
+            id: '1010',
+            subject: 'Physics',
+            location: 'London',
+            price: ' 120',
+            count: 10,
+            imagesrc: 'images/physics.png'
+        }
+
+    ]
+export default lessonproduct;
